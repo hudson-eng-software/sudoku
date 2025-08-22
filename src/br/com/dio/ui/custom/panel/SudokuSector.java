b44679fc5a800497354abb6957e4e1ad27e22720
Hudson Amorim <hudson-eng-software@gmail.com>
2025-08-22 00:44:46 -0300

    commit 1

---
+++ src/br/com/dio/ui/custom/panel/SudokuSector.java
@@ -0,0 +1,10 @@
+package br.com.dio.ui.custom.panel;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SudokuSector extends JPanel {
+    public SudokuSector(){
+        var dimension = new Dimension(170, 170);
+    }
+}
